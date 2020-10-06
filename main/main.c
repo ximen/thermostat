7725dfb880c3893bf01535dada7114341169dcfe
Сергей Новгородов <s.novgorodov@yardhotel.ru>
2020-10-06 14:30:36 +0300

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "app_config.h"
+
+void app_main(void){
+    ESP_ERROR_CHECK(app_config_init());		// Initializing and loading configuration
+}
