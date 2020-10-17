e707410292c8a3af1748585f58a0b6a78bdcb8a2
ximen <ximen@ximen.ru>
2020-10-17 16:14:35 +0300

    HTTP init

--- main/main.c
+++ main/main.c
@@ -5,10 +5,12 @@
 #include "esp_log.h"
 #include "app_config.h"
 #include "app_config_wifi.h"
+#include "app_config_http.h"
 
 #define TAG "MAIN"
 
 void app_main(void){
     ESP_ERROR_CHECK(app_config_init());		    // Initializing and loading configuration
     ESP_ERROR_CHECK(app_config_wifi_init());	// Initializing wifi according saved configuration
+    ESP_ERROR_CHECK(app_config_http_init());	// Initializing HTTP
 }
