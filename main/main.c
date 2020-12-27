89a12598d541f8f390e0dee7624696c208491acf
ximen <ximen@ximen.ru>
2020-12-27 12:47:56 +0300

    MQTT interface

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include "app_config.h"
 #include "app_config_wifi.h"
 #include "app_config_http.h"
+#include "app_config_mqtt.h"
 #include "app_config_ble_mesh.h"
 #include "esp_ble_mesh_defs.h"
 #include "esp_ble_mesh_networking_api.h"
@@ -29,12 +30,24 @@
 #define OFF_LEVEL               !ON_LEVEL
 #define BUTTON_PIN              0
 #define RESET_TIME_MS           3000
+#define CUR_TEMP_MS             2000
+#define MAX_TEMP                40
+#define MIN_TEMP                20
+
+#define MODE_STATE_TOPIC            "/mode/state"
+#define MODE_COMMAND_TOPIC          "/mode/set"
+#define TEMPERATURE_STATE_TOPIC     "/temperature/state"
+#define TEMPERATURE_COMMAND_TOPIC   "/temperature/set"
+#define TEMPERATURE_CURRENT_TOPIC   "/temperature/current"
+#define ALLOC_ERR_STR               "Error allocating buffer!"
 
 ds_bus_t    ds_buses[DS_BUS_NUMBER] = {0};
 app_config_cbs_t app_config_cbs;
 TimerHandle_t alarm_timers[CHANNEL_NUMBER];
 TimerHandle_t reset_timer;
+TimerHandle_t temperature_timer;
 bool alarms[CHANNEL_NUMBER] = {true};
+bool enabled[CHANNEL_NUMBER] = {true};
 float measurements[CHANNEL_NUMBER] = {0xFF};
 
 gpio_num_t outputs[CHANNEL_NUMBER] = {
@@ -227,7 +240,7 @@ static void worker_task(void *pvParameters){
                 ESP_LOGE(TAG, "Error retreiving hysteresis for zone %d", i);
                 alarms[i] = true;
             }
-            if(!alarms[i]){
+            if((!alarms[i])&&(enabled[i])){
                 if(measurements[i] < (target - hyst)) {
                     //printf("Turning on\n");
                     gpio_set_level(outputs[i], ON_LEVEL);
@@ -241,8 +254,166 @@ static void worker_task(void *pvParameters){
     }
 }
 
-void app_mqtt_handler_cb(){
+void app_mqtt_notify_mode(uint8_t channel){
+    char element[13] = {0};
+    sprintf(element, "base_path%d", channel + 1);
+    char *base_path;
+    app_config_getValue(element, string, &base_path);
+    if(strlen(base_path)){
+        char *mode_state_topic = (char *)calloc(strlen(base_path) + sizeof(MODE_STATE_TOPIC) + 1, sizeof(char));
+        if (!mode_state_topic){
+            ESP_LOGE(TAG, ALLOC_ERR_STR);
+            free(base_path);
+            return;
+        }
+        strncat(mode_state_topic, base_path, strlen(base_path) + 1);
+        strncat(mode_state_topic, MODE_STATE_TOPIC, sizeof(MODE_STATE_TOPIC) + 1);
+        ESP_LOGI(TAG, "Publishing mode state");
+        if(enabled[channel] && !alarms[channel]){
+            ESP_LOGI(TAG, "Topic %s, heat", mode_state_topic);
+            app_config_mqtt_publish(mode_state_topic, "heat");
+        } else {
+            ESP_LOGI(TAG, "Topic %s, off", mode_state_topic);
+            app_config_mqtt_publish(mode_state_topic, "off");
+        }
+        free(mode_state_topic);
+    }
+}
 
+void app_mqtt_notify_temp(uint8_t channel){
+    char element[13] = {0};
+    sprintf(element, "base_path%d", channel + 1);
+    char *base_path;
+    app_config_getValue(element, string, &base_path);
+    if(strlen(base_path)){
+        char *temperature_state_topic = calloc(strlen(base_path) + sizeof(TEMPERATURE_STATE_TOPIC) + 1, sizeof(char));
+        if (temperature_state_topic) {
+            strncat(temperature_state_topic, base_path, strlen(base_path) + 1);
+            strncat(temperature_state_topic, TEMPERATURE_STATE_TOPIC, sizeof(TEMPERATURE_STATE_TOPIC) + 1);
+            char zone_temp_str[13];
+            sprintf(zone_temp_str, "zone%d_temp", channel + 1);
+            uint8_t value;
+            char value_str[4];
+            esp_err_t err = app_config_getValue(zone_temp_str, int8, &value);
+            if(err == ESP_OK){
+                sprintf(value_str, "%d", value);
+                ESP_LOGI(TAG, "Publishing temperature state, topic %s, value %s", temperature_state_topic, value_str);
+                app_config_mqtt_publish(temperature_state_topic, value_str);
+            } else {
+                ESP_LOGW(TAG, "Error retreiving temperature for zone %d", channel + 1);
+            }
+        } else {
+            ESP_LOGE(TAG, ALLOC_ERR_STR);
+            free(base_path);
+        }
+        free(temperature_state_topic);
+    }
+}
+
+void app_mqtt_handler_cb(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data){
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_client_handle_t client = event->client;
+    switch (event->event_id) {
+            case MQTT_EVENT_CONNECTED:
+            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+            for(uint8_t i=0; i<CHANNEL_NUMBER; i++){
+                char element[11] = {0};
+                sprintf(element, "base_path%d", i + 1);
+                char *base_path;
+                esp_err_t err = app_config_getValue(element, string, &base_path);
+                ESP_LOGI(TAG,"%s", base_path);
+                if (strlen(base_path) > 0){
+                    char *mode_command_topic = (char *)calloc(strlen(base_path) + sizeof(MODE_COMMAND_TOPIC) + 1, sizeof(char));
+                    if(!mode_command_topic){
+                        ESP_LOGE(TAG, ALLOC_ERR_STR);
+                        free(base_path);
+                        return;
+                    }
+                    char *temperature_command_topic= (char *)calloc(strlen(base_path) + sizeof(TEMPERATURE_COMMAND_TOPIC) + 1, sizeof(char));
+                    if(!mode_command_topic){
+                        ESP_LOGE(TAG, ALLOC_ERR_STR);
+                        free(mode_command_topic);
+                        free(base_path);
+                        return;
+                    }
+                    strncat(mode_command_topic, base_path, strlen(base_path) + 1);
+                    strncat(temperature_command_topic, base_path, strlen(base_path) + 1);
+                    strncat(mode_command_topic, MODE_COMMAND_TOPIC, sizeof(MODE_COMMAND_TOPIC) + 1);
+                    strncat(temperature_command_topic, TEMPERATURE_COMMAND_TOPIC, sizeof(TEMPERATURE_COMMAND_TOPIC) + 1);
+                    ESP_LOGI(TAG, "Subscribing %s", mode_command_topic);
+                    esp_mqtt_client_subscribe(client, mode_command_topic, 0);
+                    ESP_LOGI(TAG, "Subscribing %s", temperature_command_topic);
+                    esp_mqtt_client_subscribe(client, temperature_command_topic, 0);
+                    free(mode_command_topic);
+                    free(temperature_command_topic);
+
+                    app_mqtt_notify_mode(i);
+                    app_mqtt_notify_temp(i);
+                    //xTimerStart(temperature_timer, 0);
+                }
+            }
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+            for(uint8_t i=0; i < CHANNEL_NUMBER; i++){
+                char element[13] = {0};
+                sprintf(element, "base_path%d", i + 1);
+                char *base_path;
+                app_config_getValue(element, string, &base_path);
+                if (strlen(base_path) > 0){
+                    char mode_command_topic[58] = {0};
+                    char temperature_command_topic[58] = {0};
+                    strncat(mode_command_topic, base_path, strlen(base_path) + 1);
+                    strncat(temperature_command_topic, base_path, strlen(base_path) + 1);
+                    strncat(mode_command_topic, MODE_COMMAND_TOPIC, sizeof(MODE_COMMAND_TOPIC) + 1);
+                    strncat(temperature_command_topic, TEMPERATURE_COMMAND_TOPIC, sizeof(TEMPERATURE_COMMAND_TOPIC) + 1);
+                    if(strncmp(event->topic, mode_command_topic, event->topic_len) == 0){
+                        if(strncmp(event->data, "off", event->data_len) == 0){
+                            ESP_LOGI(TAG, "Disabling channel %d", i);
+                            enabled[i] = false;
+                            app_mqtt_notify_mode(i);
+                            gpio_set_level(outputs[i], OFF_LEVEL);
+                        } else if(strncmp(event->data, "heat", event->data_len) == 0){    
+                            ESP_LOGI(TAG, "Enabling channel %d", i);
+                            enabled[i] = true;
+                            app_mqtt_notify_mode(i);
+                        } else {
+                            ESP_LOGW(TAG, "Unrecognized mode command");
+                        }
+                        break;
+                    } else if(strncmp(event->topic, temperature_command_topic, event->topic_len) == 0){
+                        char val_str[4] = {0};
+                        strncpy(val_str, event->data, (event->data_len > 3)?(3):(event->data_len));
+                        long val = strtol(val_str, NULL, 10);
+                        if((val > MAX_TEMP) || (val < MIN_TEMP)){
+                            ESP_LOGW(TAG, "Exceeds temperature range %d", (int)val);
+                        } else {
+                            char zone_temp_str[11];
+                            sprintf(zone_temp_str, "zone%d_temp", i + 1);
+                            ESP_LOGI(TAG, "Setting zone %d temp to %d", i, (uint8_t)val);
+                            esp_err_t err = app_config_setValue(zone_temp_str, &val);
+                            if(err != ESP_OK){
+                                ESP_LOGW(TAG, "Error setting temperature!");
+                            } else {
+                                app_mqtt_notify_temp(i);
+                            }
+                        }
+                        break;
+                    } else {
+                        ESP_LOGW(TAG, "Unknown MQTT topic");
+                    }
+                }
+            }
+            break;
+            case MQTT_EVENT_DISCONNECTED:
+                //xTimerStop(temperature_timer, 0);
+            break;
+        default:
+            break;
+    }
 }
 
 void alarm_timer_cb(TimerHandle_t xTimer){
@@ -257,6 +428,41 @@ void alarm_timer_cb(TimerHandle_t xTimer){
 
 void reset_timer_cb(TimerHandle_t xTimer){
     app_config_erase();
+}
+
+void temperature_timer_cb(TimerHandle_t xTimer)
+{
+    bool config_mqtt_enable;
+    app_config_getBool("ble_mesh_enable", &config_mqtt_enable);
+    if (config_mqtt_enable)
+    {
+        for (uint8_t i = 0; i < CHANNEL_NUMBER; i++)
+        {
+            if (!alarms[i])
+            {
+                char element[13];
+                sprintf(element, "base_path%d", i + 1);
+                char *base_path;
+                app_config_getValue(element, string, &base_path);
+                char *cur_temp_topic = calloc(strlen(base_path) + sizeof(TEMPERATURE_CURRENT_TOPIC) + 1, sizeof(char));
+                if (cur_temp_topic)
+                {
+                    strncat(cur_temp_topic, base_path, strlen(base_path) + 1);
+                    strncat(cur_temp_topic, TEMPERATURE_CURRENT_TOPIC, sizeof(TEMPERATURE_CURRENT_TOPIC) + 1);
+                    char value_str[4];
+                    sprintf(value_str, "%2.1f", measurements[i]);
+                    ESP_LOGI(TAG, "Publishing current temperature %s", value_str);
+                    app_config_mqtt_publish(cur_temp_topic, value_str);
+                    free(cur_temp_topic);
+                }
+                else
+                {
+                    ESP_LOGE(TAG, ALLOC_ERR_STR);
+                    continue;
+                }
+            }
+        }
+    }
 }
 
 static void IRAM_ATTR gpio_isr_handler(void* arg){
@@ -319,7 +525,6 @@ void app_main(void){
          }
     }
 
-
     ESP_LOGI(TAG, "Starting worker thread");
     if (xTaskCreate(worker_task, "worker", TASK_STACK_SIZE, NULL, 1, NULL) != pdPASS){
         ESP_LOGE(TAG, "Error creating task!");
@@ -331,6 +536,12 @@ void app_main(void){
         ESP_LOGE(TAG, "Error creating task!");
         abort();
     }
-
+    bool config_mqtt_enable;
+    app_config_getBool("ble_mesh_enable", &config_mqtt_enable);
+    if (config_mqtt_enable){
+        ESP_LOGI(TAG, "Starting temperature notification timer");
+        temperature_timer = xTimerCreate("temperature_timer", CUR_TEMP_MS / portTICK_PERIOD_MS, pdTRUE, NULL, temperature_timer_cb);
+        xTimerStart(temperature_timer, 0);
+    }
 }
 
